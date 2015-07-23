98a2ee7adead2a2811ec4ced99b78bb7eefe1c31
Austin Heywood <austinheywood@gmail.com>
2015-07-23 09:23:20 -0700

    Updates to base source/index

--- assets/js/meplusmore.js
+++ assets/js/meplusmore.js
@@ -1,6 +1,38 @@
 // meplusmore scripts
 
 
+// On Browser Window Resize Add/Remove Collapse Class to Main Navigation
+window.onresize = function() {
+  if (window.innerWidth >= 768) {
+    $('#menuNav').removeClass('collapse');
+  } else if (window.innerWidth <= 767) {
+    $('#menuNav').addClass('collapse');
+  }
+};
+
+
+// On Page Load Add/Remove Collapse Class to Main Navigation
+$(document).ready(function(e) { 
+  if (window.innerWidth >= 768) {
+    $('#menuNav').removeClass('collapse');
+  } else if (window.innerWidth <= 767) {
+    $('#menuNav').addClass('collapse');
+  }
+});
+
+
+// Mobile Navigation - Toggle bars / close icon
+$('#mobileNavControl').click(function() {
+  $("#mobileNavControl i").toggleClass("fa-bars");
+  $("#mobileNavControl i").toggleClass("fa-times");
+});
+
+
+
+
+// HERO Parallax Images
 $('#hero.features').parallax({imageSrc: 'assets/img/meplusmore_hero-features.jpg'});
 
 
+// WOW Animation Add-on
+new WOW().init();
